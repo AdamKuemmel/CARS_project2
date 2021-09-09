eea0af1d3fe49852321a6f4981b6da452accb382
stephaniecjohansson <7steph7@gmail.com>
2021-09-09 12:54:39 -0600

    new stuff

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const sequelize = require("./config/connection");
 
-const models = require("./models");
+// const models = require("./models");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
